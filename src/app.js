2d5ccb3c04d11cd5d0976d3ffbff27dab852d992
Ando <a.r.north23@gmail.com>
2022-12-06 18:04:35 +0000

    1) added docker-compose for database to be run in a separate container 2) added mysql for interfacing with database 3) updated GET,, POST, PUT and DELETE response to interface with  database 4) updated test_apy.py script

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 const Joi = require("joi"); // npm i joi@13.1.0
 const express = require("express"); // npm i express
 const bodyParser = require("body-parser"); // npm i body-parser
+const { createPool } = require("mysql"); // npm i mysql
 
 const app = express();
+
 const jsonParser = bodyParser.json();
 app.use(jsonParser);
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(urlencodedParser);
+
+const pool = createPool({
+  host: "localhost",
+  user: "cpp_rest_user",
+  password: "password123",
+  database: "cpp_rest_db",
+  connectionLimit: 10,
+});
 
 const videos = [
   //   { id: 1, likes: 10, name: "ando's funny video", views: 121 },
@@ -16,14 +26,23 @@ const videos = [
 const endPoint = "/videos/:id(\\d+)";
 // get videos
 app.get("/videos", (req, res) => {
-  res.send(videos);
+  let sql = "select * from videos";
+  pool.query(sql, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
 });
+// get video at id
 app.get(endPoint, (req, res) => {
-  const video = videos.find((v) => v.id === parseInt(req.params.id));
-  if (!video) {
-    return res.status(404, "The video id was not found");
-  }
-  res.send(video);
+  let sql = "select * from videos where id = ?";
+  pool.query(sql, [req.params.id], (err, results) => {
+    if (err) throw err;
+    if (!results[0]) {
+      res.json({ status: "Not Found!" });
+    } else {
+      res.json(results[0]);
+    }
+  });
 });
 // add video
 app.post(endPoint, (req, res) => {
@@ -45,10 +64,17 @@ app.post(endPoint, (req, res) => {
     views: parseInt(req.body.views),
   };
 
-  videos.push(video);
-  res.send(video);
+  let sql = "insert into videos (id, name, likes, views) values (?,?,?,?)";
+  pool.query(
+    sql,
+    [req.params.id, req.body.name, req.body.likes, req.body.views],
+    (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    }
+  );
 });
-// update video
+// update video - TODO: fix this
 app.put(endPoint, (req, res) => {
   const video = videos.find((v) => v.id === parseInt(req.params.id));
   if (!video) {
@@ -62,28 +88,36 @@ app.put(endPoint, (req, res) => {
   };
   const result = Joi.validate(req.body, schema);
 
+  // TODO: concatenate and loop through values
   if (result.value.name) {
-    video.name = result.value.name;
+    let sql = "update videos set name = ? where id = ?";
+    pool.query(sql, [result.value.name, req.params.id], (err, results) => {
+      if (err) throw err;
+      console.log(results);
+    });
   }
   if (result.value.likes) {
-    video.likes = result.value.likes;
+    let sql = "update videos set likes = ? where id = ?";
+    pool.query(sql, [result.value.likes, req.params.id], (err, results) => {
+      if (err) throw err;
+      console.log(results);
+    });
   }
   if (result.value.views) {
-    video.views = result.value.views;
+    let sql = "update videos set views = ? where id = ?";
+    pool.query(sql, [result.value.views, req.params.id], (err, results) => {
+      if (err) throw err;
+      console.log(results);
+    });
   }
-
-  res.send(video);
 });
 // delete video
 app.delete(endPoint, (req, res) => {
-  const video = videos.find((v) => v.id === parseInt(req.params.id));
-  if (!video) {
-    return res.status(404, "The video id was not found");
-  }
-
-  const index = videos.indexOf(video);
-  videos.splice(index, 1);
-  res.send(video);
+  let sql = "delete from videos where id = ?";
+  pool.query(sql, [req.params.id], (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
 });
 // HTTP Server endpoint
 const defaultPort = 5000;
--- test_api.py
+++ test_api.py
@@ -6,24 +6,29 @@ data = [{"likes": 10, "name":"ando's funny video", "views" : 121},
         {"likes": 1420, "name":"how to make paper planes", "views" : 42000},
         {"likes": 23099, "name":"awesome flip", "views" : 929299}]
 
-for i in range(len(data)):
-    request_str=BASE + "videos/"+str(i)
-    response = requests.post(request_str, data[i])
-    print(request_str)
-    print(response.json())
+# for i in range(len(data)):
+#     request_str=BASE + "videos/"+str(i)
+#     response = requests.post(request_str, data[i])
+#     print(request_str)
+#     print(response.json())
 
-raw_input()
+idx=0
+idx+=1
+raw_input(idx)
 request_str=BASE + "videos/2"
 print(request_str)
 response = requests.get(request_str)
 print(response)
 print(response.json())
-raw_input()
-response = requests.put(request_str, {"views": 130})
-print(response.json())
-raw_input()
+idx+=1
+raw_input(idx)
 response = requests.delete(request_str)
 print(response.json())
-raw_input()
+idx+=1
+raw_input(idx)
 response = requests.post(request_str, data[2])
+print(response.json())
+idx+=1
+raw_input(idx)
+response = requests.put(request_str, {"views": 130})
 print(response.json())
